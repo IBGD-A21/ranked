e7184816f80d46f53b9449a3c1955f9deb3ead0b
RodrigoTrevino96 <61522302+RodrigoTrevino96@users.noreply.github.com>
2023-08-08 22:35:41 -0600

    on laptop commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,12 @@ function App() {
     { id: 4, name: "Lorem Ipsum 5", stars: Math.round(Math.random() * 10) },
   ];
 
+  const handleAddStar = (props: any) => {
+    // console.log(props);
+    members[props].stars++;
+    console.log(members[props].stars);
+  }
+
   const renderMemberList = () => {
     return members.map((member, index) => {
       return (
@@ -18,10 +24,13 @@ function App() {
           key={index}
           name={member.name}
           stars={member.stars}
+          id={index}
+          onAddStar={handleAddStar}
         />
       );
     });
   };
+
   return <div className="App">{renderMemberList()}</div>;
 }
 
--- src/components/CardMember/CardMember.tsx
+++ src/components/CardMember/CardMember.tsx
@@ -1,23 +1,47 @@
-import { Avatar, Card, CardContent, CardHeader } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, IconButton } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
 import { stringAvatar } from "../../utils";
 import { MemberCardProps } from "./types";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const MemberCard = (props: MemberCardProps) => {
   const {
     name = "Lorem Ipsum",
     stars,
+    id,
+    onAddStar,
   } = props;
 
   const renderStars = () => {
     return [...Array(stars)].map((_, i) => <StarIcon key={i} />);
   };
 
+  const onAddStarChild = () => {
+    onAddStar && onAddStar(id)
+  }
+
   return (
     <>
       <Card>
         <CardHeader avatar={<Avatar {...stringAvatar(name)} />} title={name} />
         <CardContent>{renderStars()}</CardContent>
+        <CardActions>
+        <IconButton
+          size="small"
+          color="primary"
+          onClick={onAddStarChild}
+        >
+         <AddIcon />
+        </IconButton>
+        <Button
+          size="small"
+          color="primary"
+          // onClick={onAddStar}
+        >
+         <RemoveIcon />
+        </Button>
+      </CardActions>
       </Card>
     </>
   );
