ace52ac1fe1577ce204b6c2c61c0e815625c4ca1
Rodrigo Treviño <ri.trevino96@gmail.com>
2023-08-09 20:54:37 -0600

    star initial animation

--- src/components/CardMember/styles.ts
+++ src/components/CardMember/styles.ts
@@ -10,6 +10,14 @@ const tinkleStars = (rotate: number) =>
     "100%": { transform: `rotate(-${rotate}deg)` },
   });
 
+const appear = (initialScale: number) =>
+  keyframes({
+    "0%": { transform: `scale(${initialScale})` },
+    "50%": { transform: `scale(${0.75})` },
+    "75%": { transform: `scale(${1.15})` },
+    "100%": { transform: `scale(${1})` },
+  });
+
 export const StyledCard = styled(Card)({
   display: "flex",
   alignItems: "center",
@@ -32,4 +40,8 @@ export const StyledStarIcon = styled(Star)({
   color: "#f1e22d",
   animation: `${tinkleStars(10)} 1s ease-in-out infinite`,
   transformOrigin: "bottom center",
+  path: {
+    animation: `${appear(1.5)} 0.5s`,
+    transformOrigin: "center",
+  },
 });
