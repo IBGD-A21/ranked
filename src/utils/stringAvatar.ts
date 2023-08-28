89efa150d267e35d4e94d6f3954033375322450b
Rodrigo Treviño <ri.trevino96@gmail.com>
2023-08-27 20:34:11 -0600

    fixing avatar text accepting one word

--- src/utils/stringAvatar.ts
+++ src/utils/stringAvatar.ts
@@ -1,8 +1,13 @@
 import { stringToColor } from "./stringToColor";
 
 export const stringAvatar = (name: string) => {
+  const splittedString = name.split(" ");
+  const avatar = splittedString.length === 1
+  ? `${splittedString[0][0]}`
+  : `${splittedString[0][0]}${splittedString[1][0]}`;
+
   return {
     sx: { bgcolor: stringToColor(name) },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    children: avatar,
   };
 };
